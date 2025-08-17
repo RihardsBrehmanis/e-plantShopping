591b41c21a5fa29b026b696404bd62d26809f2fd
RihardsBrehmanis <rihardsbrehmanis@gmail.com>
2025-08-17 11:03:13 -0400

    fix: Correct Vite base URL and add deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  // Change the base path to your correct repository name
+  base: "/e-plantShopping/",
   plugins: [react()],
 })
